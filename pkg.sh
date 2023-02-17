9a105ae02741f7cab76d77cbf9cd7aeb50dec58b
Tuhin Ahmed <117445766+JessicamMnQShar@users.noreply.github.com>
2023-02-17 09:04:08 +0600

    Add files via upload

---
+++ pkg.sh
@@ -0,0 +1,70 @@
+clear
+echo "
+
+
+████████ ██    ██ ██   ██ ██ ███    ██ 
+   ██    ██    ██ ██   ██ ██ ████   ██ 
+   ██    ██    ██ ███████ ██ ██ ██  ██ 
+   ██    ██    ██ ██   ██ ██ ██  ██ ██ 
+   ██     ██████  ██   ██ ██ ██   ████ 
+                                       
+                                       
+" lolcat
+echo "======================================="
+echo "  Tools Name : BASIC PACKAGE INSTALL"
+echo "  Author : TUHIN AHMED"
+echo "   FB ACCOUNT: MOHAMMAD TUHIN AHMED"
+echo" FB ACCOUNT LINK: https://www.facebook.com/tuhin.vai.nasa.hocker.xode.na"
+echo "======================================="
+termux-setup-storage
+
+apt update
+
+apt upgrade -y
+
+pkg install python -y
+
+pkg install python2 -y
+
+pkg install python3 -y
+
+pkg install git -y
+
+pkg install bash -y
+
+pkg install figlet -y
+
+pkg install toilet -y
+
+pkg install nano -y
+
+pkg install php -y
+
+pkg install pip -y
+
+pkg install pip2 -y
+
+pip2 install requests as -y
+
+pip install future -y
+
+pip2 install requirements -y
+
+apt install ruby -y
+
+apt install openssh -y 
+
+apt install wget -y
+
+apt install curl -y
+
+apt install proof -y
+
+pkg install proot-distro
+
+Pkg install fish 
+
+
+echo ""
+echo ""
+echo "All Package Installed"
